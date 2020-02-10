a35ccec8281b0e1a08af1f73c712d4cf108ed609
darekaze <darekaze@gmail.com>
2020-02-11 02:47:35 +0800

    add missing packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,7 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site. Change this
       },
     },
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
