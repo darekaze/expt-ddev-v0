24dbfd080376b596d34a173ad35faccf0cfd2144
darekaze <darekaze@gmail.com>
2020-02-29 08:40:44 +0800

    fix: layout style

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -12,7 +12,8 @@ export const Layout: React.FC = ({ children }) => (
       as='main'
       width='100%'
       minHeight='100vh'
-      maxWidth='1440px'
+      maxWidth='1600px'
+      margin='0px auto'
       px={['25px', '50px', '100px', '150px']}>
       {children}
     </Box>
